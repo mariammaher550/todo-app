ec106871d7dc79812d671851f93bba8edd118b66
mariammaher550 <mariammaher550@gmail.com>
2022-04-02 00:35:38 +0300

    demo app

---
+++ components/Task.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+ 
+const Task = (props) => {
+    
+    return(
+        <View style = {styles.item}>
+            <View style = {styles.itemLeft}>
+            <TouchableOpacity style = {styles.square}></TouchableOpacity> 
+                <Text style = {styles.itemText}> {props.text} </Text>
+            </View>
+            <View style = {styles.circular}></View>
+        </View>    
+    );
+}
+
+const styles = StyleSheet.create({
+ itemText : {
+     color : 'black',
+     maxWidth : '80%'
+ },
+ item : {
+     backgroundColor : '#fff',
+     padding : 15,
+     borderRadius : 20,
+     flexDirection : 'row',
+     alignItems: 'center',
+     justifyContent : 'space-between',
+     marginBottom : 20,
+
+ },
+ itemLeft : {
+     flexDirection: 'row',
+     alignItems: 'center',
+     flexWrap : 'wrap',
+ },
+ square : {
+     width : 24,
+     height : 24,
+     backgroundColor : '#55BCF6',
+     opacity : 0.4,
+     borderRadius: 5,
+     marginRight : 15,
+
+ },
+ circular : {
+     width: 12,
+     height: 12,
+     borderColor : '#55BCF6',
+     borderWidth : 2,
+     borderRadius : 5
+ }
+});
+
+export default Task;
